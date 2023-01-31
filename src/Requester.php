0b759523e5f29f628818b261c22dcef5954968f5
Yohann <yohann@humantocomputer.com>
2023-01-31 14:18:41 +0100

    exception

--- src/Requester.php
+++ src/Requester.php
@@ -123,7 +123,7 @@ class Requester
             dump($response);
         }
 
-        throw new UnexpectedValueException($response);
+        throw new \Exception($response->errstr, $response->errno);
 
     }
 
